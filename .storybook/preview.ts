30705f55984faccd905f55c4ea0b266f43b1d5fa
choisuhyeok1255 <csh300029@gmail.com>
2023-11-20 23:43:00 +0900

    feat: storybook preview.ts toc 추가 (#3)

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -2,6 +2,7 @@ import type { Preview } from "@storybook/react";
 
 const preview: Preview = {
   parameters: {
+    docs: { toc: true },
     actions: { argTypesRegex: "^on[A-Z].*" },
     controls: {
       matchers: {
